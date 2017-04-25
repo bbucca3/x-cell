866229d837c8f61648635daf9e75e8268fe15558
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-04-25 13:55:32 -0400

    set public/index.html

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-//This is the key line that will ask Express to serve our 
-//HTML, CSS, and client-side JS files:
+//This is the key line that will ask Express to serve our HTML, CSS, and client-side JS files:
 app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 
